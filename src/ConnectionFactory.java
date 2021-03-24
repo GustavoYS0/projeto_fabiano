e65c7bc7f6a1c73e21b81a83575892ae8dd9378a
Pedro <pedro.trabalhos@outlook.com>
2021-03-23 22:05:12 -0300

    Factory

--- src/ConnectionFactory.java
+++ src/ConnectionFactory.java
@@ -1,3 +1,19 @@
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+import java.sql.Connection;
+
 public class ConnectionFactory {
      
+     private final String DRIVER = "com.mysql.jdbc.Driver";
+     private final String URL = "jdbc:mysql://localhost:3306/banco_trabalho";
+     private final String USER = "root"; // ver o de vcs
+     private final String PASS = "164087"; // ver o de vcs
+
+     public Connection getConnection(){
+
+          try {
+               
+          } catch (Exception e) {
+               e.getMessage();
+          }
 }
